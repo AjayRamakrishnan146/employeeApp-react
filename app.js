31f5299b86dbc5dc0d0f0c3849192a8cb93c55cf
AjayRamakrishnan146 <ajayramakrishnan128@gmail.com>
2023-07-25 22:50:23 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,37 @@
+const express= require('express');
+const app = new express();
+const morgan = require('morgan');
+app.use(morgan('dev'));
+require('dotenv').config();
+
+var cors = require('cors');
+app.use(cors());
+
+require('./db/employeeDtata');
+
+const path = require('path'); 
+app.use(express.static(path.join(__dirname,'/build')));
+
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "X-Requested-With,content-type ");
+    res.setHeader("Access-Control-Allow-Credentials", true);
+    next();
+})
+
+
+const api= require('./routes/employeeRoute');
+app.use('/api',api);
+
+
+
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname,'/build/index.html'));
+ });
+
+
+PORT= 5000;
+app.listen(PORT,()=>{
+    console.log(`The server is running on ${PORT}`)
+})
